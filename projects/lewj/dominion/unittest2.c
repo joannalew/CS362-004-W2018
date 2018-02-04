2ce02021f2397b4596dc8d913cdcc5e1379afa04
Joanna <lewj@oregonstate.edu>
2018-02-03 16:51:06 -0800

    unittest2: sudoAssert

--- projects/lewj/dominion/unittest2.c
+++ projects/lewj/dominion/unittest2.c
@@ -19,8 +19,17 @@
 // set NOISY_TEST to 0 to remove printfs from output
 #define NOISY_TEST 1
 
+int sudoAssert(int test){
+    if (test)
+        return 0;
+    else
+        return 1;
+}
 
 int main() {
+    int checker = 0;
+    int totalFailed = 0;
+    
     int x, y, z, r;
     int numPlayer = 4;
     int seed = 1000;
@@ -36,10 +45,14 @@ int main() {
     r = initializeGame(numPlayer, k, seed, &G);         // initialize a new game
     
     G.supplyCount[province] = 0;
-#if (NOISY_TEST == 1)
-    printf("testing empty province pile\n");
-#endif
-    assert(isGameOver(&G) == 1);
+    printf("Testing empty province pile\n");
+
+    checker += sudoAssert((isGameOver(&G) == 1));
+    if (checker > 0){
+        checker = 0;
+        printf("Empty province pile failed\n");
+        totalFailed++;
+    }
     
     // test for game over if 3 supplies are empty
     // try every combination of empty supply cards
@@ -47,6 +60,7 @@ int main() {
     int y_max = x_max + 1;
     int z_max = y_max + 1;
     
+    printf("Testing empty supply piles\n");
     for (x = 0; x < x_max; x++){
         for (y = x+1; y < y_max; y++){
             for (z = y+1; z < z_max; z++){
@@ -57,23 +71,34 @@ int main() {
                 G.supplyCount[k[y]] = 0;
                 G.supplyCount[k[z]] = 0;
 #if (NOISY_TEST == 1)
-                printf("testing empty supply piles: [%d][%d][%d]\n", x, y, z);
+                printf("Testing empty supply piles: [%d][%d][%d]\n", x, y, z);
 #endif
-                assert(isGameOver(&G) == 1);
+                checker += sudoAssert((isGameOver(&G) == 1));
             }
         }
+    }
+    
+    if (checker > 0){
+        checker = 0;
+        printf("Empty supply piles failed\n");
+        totalFailed++;
     }
     
     // fixed test: new game -> not game over
     memset(&G, 23, sizeof(struct gameState));           // clear the game state
     r = initializeGame(numPlayer, k, seed, &G);         // initialize a new game
-#if (NOISY_TEST == 1)
-    printf("testing new game is NOT game over\n");
-#endif
-    assert(isGameOver(&G) == 0);
+
+    printf("Testing new game is NOT game over\n");
     
+    checker += sudoAssert((isGameOver(&G) == 0));
+    if (checker > 0){
+        checker = 0;
+        printf("New game is NOT game over failed\n");
+        totalFailed++;
+    }
     
-    printf("All tests passed!\n");
+    if (totalFailed == 0)
+        printf("All tests passed!\n");
 
     return 0;
 }
